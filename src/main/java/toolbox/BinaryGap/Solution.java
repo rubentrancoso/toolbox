0f83bb70312eaf7e0cd64d01a10edb57423a0364
Ruben Trancoso <rpmartins@indracompany.com>
2017-05-02 14:42:03 -0300

    BInaryGap

--- src/main/java/toolbox/BinaryGap/Solution.java
+++ src/main/java/toolbox/BinaryGap/Solution.java
@@ -2,6 +2,20 @@ package toolbox.BinaryGap;
 
 public class Solution {
     public int solution(int N) {
-		return N;
+    	int n = N;
+    	int longest = 0;
+    	int count = 0;
+    	while((n != 0) && (n & 1) != 1) n >>= 1;
+    	while(n != 0) {
+    		if((n & 1) == 1) {
+    			if(count>longest)
+    				longest = count;
+    			count = 0;
+    		} else {
+    			count++;
+    		}
+    		n >>= 1;
+    	}
+		return longest;
     }
 }
---
+++ src/main/java/toolbox/BinaryGap/SolutionTest.java
@@ -0,0 +1,66 @@
+package toolbox.BinaryGap;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class SolutionTest {
+
+	private Solution solution;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		solution = new Solution();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@org.junit.Test
+	public void testSolution() {
+		int N, result;
+		N = Integer.parseInt("0", 2);
+		result = solution.solution(N);
+		assertEquals(result,0);
+		N = Integer.parseInt("1", 2);
+		result = solution.solution(N);
+		assertEquals(result,0);
+		N = Integer.parseInt("10", 2);
+		result = solution.solution(N);
+		assertEquals(result,0);
+		N  = Integer.parseInt("11", 2);
+		result = solution.solution(N);
+		assertEquals(result,0);
+		N  = Integer.parseInt("101", 2);
+		result = solution.solution(N);
+		assertEquals(result,1);
+		N  = Integer.parseInt("1010", 2);
+		result = solution.solution(N);
+		assertEquals(result,1);
+		N  = Integer.parseInt("10010", 2);
+		result = solution.solution(N);
+		assertEquals(result,2);
+		N  = Integer.parseInt("10100", 2);
+		result = solution.solution(N);
+		assertEquals(result,1);
+		N = Integer.parseInt("1000100", 2);
+		result = solution.solution(N);
+		assertEquals(result,3);
+		N  = Integer.parseInt("1001000", 2);
+		result = solution.solution(N);
+		assertEquals(result,2);
+	}
+
+}

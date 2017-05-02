5857a99dac0aed66840cbda2771c3387b67358f1
Ruben Trancoso <rpmartins@indracompany.com>
2017-05-02 19:36:13 -0300

    FrogJmp template

---
+++ src/main/java/toolbox/codility/lesson3/timecomplexity/FrogJmp/Solution.java
@@ -0,0 +1,14 @@
+package toolbox.codility.lesson3.timecomplexity.FrogJmp;
+
+
+// https://codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+
+// Complexity:
+// expected worst-case time complexity is O(1);
+// expected worst-case space complexity is O(1).
+
+public class Solution {
+    public int solution(int N) {
+		return N;
+    }
+}

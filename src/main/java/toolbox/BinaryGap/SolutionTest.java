863e3ca1f4785e043536363845a23cd00370c554
Ruben Trancoso <rpmartins@indracompany.com>
2017-05-02 14:44:05 -0300

    format fix

--- src/main/java/toolbox/BinaryGap/SolutionTest.java
+++ src/main/java/toolbox/BinaryGap/SolutionTest.java
@@ -10,7 +10,7 @@ import org.junit.BeforeClass;
 public class SolutionTest {
 
 	private Solution solution;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -33,34 +33,34 @@ public class SolutionTest {
 		int N, result;
 		N = Integer.parseInt("0", 2);
 		result = solution.solution(N);
-		assertEquals(result,0);
+		assertEquals(result, 0);
 		N = Integer.parseInt("1", 2);
 		result = solution.solution(N);
-		assertEquals(result,0);
+		assertEquals(result, 0);
 		N = Integer.parseInt("10", 2);
 		result = solution.solution(N);
-		assertEquals(result,0);
-		N  = Integer.parseInt("11", 2);
+		assertEquals(result, 0);
+		N = Integer.parseInt("11", 2);
 		result = solution.solution(N);
-		assertEquals(result,0);
-		N  = Integer.parseInt("101", 2);
+		assertEquals(result, 0);
+		N = Integer.parseInt("101", 2);
 		result = solution.solution(N);
-		assertEquals(result,1);
-		N  = Integer.parseInt("1010", 2);
+		assertEquals(result, 1);
+		N = Integer.parseInt("1010", 2);
 		result = solution.solution(N);
-		assertEquals(result,1);
-		N  = Integer.parseInt("10010", 2);
+		assertEquals(result, 1);
+		N = Integer.parseInt("10010", 2);
 		result = solution.solution(N);
-		assertEquals(result,2);
-		N  = Integer.parseInt("10100", 2);
+		assertEquals(result, 2);
+		N = Integer.parseInt("10100", 2);
 		result = solution.solution(N);
-		assertEquals(result,1);
+		assertEquals(result, 1);
 		N = Integer.parseInt("1000100", 2);
 		result = solution.solution(N);
-		assertEquals(result,3);
-		N  = Integer.parseInt("1001000", 2);
+		assertEquals(result, 3);
+		N = Integer.parseInt("1001000", 2);
 		result = solution.solution(N);
-		assertEquals(result,2);
+		assertEquals(result, 2);
 	}
 
 }

5977dd0eaf0e6ae51569de716ebf5435d579e7b4
Ruben Trancoso <rpmartins@indracompany.com>
2017-05-02 17:14:29 -0300

    PermMissingElem Big O constraints

--- src/main/java/toolbox/codility/lesson3/timecomplexity/PermMissingElem/Solution.java
+++ src/main/java/toolbox/codility/lesson3/timecomplexity/PermMissingElem/Solution.java
@@ -1,5 +1,12 @@
 package toolbox.codility.lesson3.timecomplexity.PermMissingElem;
 
+//Complexity:
+
+// expected worst-case time complexity is O(N);
+// expected worst-case space complexity is O(1), 
+// beyond input storage (not counting the storage required for input arguments).
+// Elements of input arrays can be modified.
+
 public class Solution {
 	public int solution(int[] A) {
 		return 4;

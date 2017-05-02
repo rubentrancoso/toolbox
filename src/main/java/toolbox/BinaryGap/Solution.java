26864460a9703485ef98f193ee833fd848e7b166
Ruben Trancoso <rpmartins@indracompany.com>
2017-05-02 15:02:10 -0300

    code cleaning

--- src/main/java/toolbox/BinaryGap/Solution.java
+++ src/main/java/toolbox/BinaryGap/Solution.java
@@ -2,17 +2,16 @@ package toolbox.BinaryGap;
 
 public class Solution {
 	public int solution(int N) {
-		int n = N;
 		// Initialize the longest value and counter to 0
 		int longest = 0;
 		int count = 0;
 		// While n is not 0, shift to the right until you find a 1  
-		while ((n != 0) && (n & 1) != 1)
-			n >>= 1;
+		while ((N != 0) && (N & 1) != 1)
+			N >>= 1;
 		// When the shift resulted in a non zero
-		while (n != 0) {
-			if ((n & 1) == 1) {
-				// If 1 is found and the counter is greater than the longest, use it
+		while (N != 0) {
+			if ((N & 1) == 1) {
+				// If 1 was found and the counter is greater than the longest, use it
 				// and start counting (again)
 				if (count > longest)
 					longest = count;
@@ -22,7 +21,7 @@ public class Solution {
 				count++;
 			}
 			// shit to the right
-			n >>= 1;
+			N >>= 1;
 		}
 		return longest;
 	}

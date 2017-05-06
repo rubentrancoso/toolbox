1d2b8a5bf9bb197630d6355eefbc7d88f9634f6f
Ruben Paulo Martins Trancoso <rubentrancoso@gmail.com>
2017-05-06 20:56:29 -0300

    FrogRiver solution

--- src/main/java/toolbox/codility/lesson4/countingelements/FrogRiverOne/Solution.java
+++ src/main/java/toolbox/codility/lesson4/countingelements/FrogRiverOne/Solution.java
@@ -1,7 +1,23 @@
 package toolbox.codility.lesson4.countingelements.FrogRiverOne;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
-    public int solution(int N) {
-		return N;
+	// https://codility.com/demo/results/trainingSK3UVU-5FM/
+    public int solution(int X, int[] A) {
+    	Set<Integer> pos = new HashSet<Integer>();
+    	for(int i=1;i<=X;i++) {
+    		pos.add(i);
+    	}
+    	int result = -1;
+    	int size = A.length;
+    	for(int second=0;second<size;second++) {
+    		pos.remove(A[second]);
+    		if(pos.size()==0)
+    			return second;
+    	}
+		return result;
     }
+
 }
--- src/main/java/toolbox/codility/lesson4/countingelements/FrogRiverOne/SolutionTest.java
+++ src/main/java/toolbox/codility/lesson4/countingelements/FrogRiverOne/SolutionTest.java
@@ -1,6 +1,6 @@
 package toolbox.codility.lesson4.countingelements.FrogRiverOne;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,7 +11,7 @@ import org.junit.Test;
 public class SolutionTest {
 
 	private Solution solution;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -31,7 +31,19 @@ public class SolutionTest {
 
 	@Test
 	public void testSolution() {
-		fail("Not yet implemented"); // TODO
+		int X = 5;
+		int N = 0;
+		int[] A = new int[8];
+		A[0] = 1;
+		A[1] = 3;
+		A[2] = 1;
+		A[3] = 4;
+		A[4] = 2;
+		A[5] = 3;
+		A[6] = 5;
+		A[7] = 4;
+		int result = solution.solution(X, A);
+		assertEquals(6, result);
 	}
 
 }

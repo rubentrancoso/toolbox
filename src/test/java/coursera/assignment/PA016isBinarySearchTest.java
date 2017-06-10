347155e65f8785e6511f55dc11f3ded5a80810aa
Ruben Paulo Martins Trancoso <rubentrancoso@gmail.com>
2017-06-10 01:20:03 -0300

    binarysearch

---
+++ src/test/java/coursera/assignment/PA016isBinarySearchTest.java
@@ -0,0 +1,32 @@
+package coursera.assignment;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class PA016isBinarySearchTest {
+
+	@Test
+	public void search() {
+		int[] a =  {1, 5, 8, 12, 13};
+		int[] keys = {8, 1, 23, 1, 11};
+		int[] result = {2, 0, -1, 0, -1};
+		int[] output = new int[keys.length];
+		for(int i=0;i<keys.length;i++) {
+			output[i] = PA016isBinarySearch.binarySearch(a, keys[i]);
+		}
+		assertArrayEquals(output, result);
+	}
+	
+	@Test
+	public void search1() {
+		int[] a =  {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+		int[] keys = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+		int[] result = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1};
+		int[] output = new int[keys.length];
+		for(int i=0;i<keys.length;i++) {
+			output[i] = PA016isBinarySearch.binarySearch(a, keys[i]);
+		}
+		assertArrayEquals(output, result);
+	} 
+
+}
